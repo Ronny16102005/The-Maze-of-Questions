55c616ac9aa8ca9afffeb2abee7a7479a5f6ee15
Ronny16102005 <ronnydiazdelgado6@gmail.com>
2025-01-29 17:07:12 -0500

    Assembly

--- obj/Debug/net9.0/The Maze of Questions.AssemblyInfo.cs
+++ obj/Debug/net9.0/The Maze of Questions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The Maze of Questions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a56ba6995d6f0c964e7fa1844eb20b20ec5ac768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203ff93fbc59f303012d79cc2022cfd93fd69a6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("The Maze of Questions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The Maze of Questions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
